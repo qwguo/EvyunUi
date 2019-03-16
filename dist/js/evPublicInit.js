4245736889bce6b515254f42f99fb9fbf6d3d7b3
qwguo <75624839@qq.com>
2019-03-16 11:58:28 +0800

    'qwguo'

--- dist/js/evPublicInit.js
+++ dist/js/evPublicInit.js
@@ -9729,33 +9729,49 @@ and dependencies (minified).
 /**
  * user HTML Element add attribute
  * data-toggle="allSelect"    表示这个元素是全选元素，内部有要全选的对象
- * data-all-name="value"      表示元素内，用来单击全选的元素 input checkbox元素
- * data-target-name="hobby_1" 表示要选中或者取消选中的目标元素 input checkbox元素
+ * data-all-name="value1,value2"      表示元素内，用来单击全选的元素 input checkbox元素的name名称，多个用逗号分开
+ * data-target-name="value,value2" 表示要选中或者取消选中的目标元素 input checkbox元素元素的name名称，多个用逗号分开
+ * 其中多个all-name和target-name需要一一对应
  * */
 (function ($) {
-  $(document).on('click.allSelect', '[data-toggle="allSelect"]', function (ev) {
-    var $this = $(this),
-      clickTarget = $(ev.target);
-    if (clickTarget[0].tagName.toLowerCase() === 'input' && clickTarget.attr('type') === 'checkbox') {
-      var $thisDate = $this.data(),
-        allName = $thisDate.allName,
-        targetName = $thisDate.targetName,
-        curName = clickTarget.attr('name');
-      if (curName && (curName === targetName || curName === allName)) {
-        var flag = clickTarget.prop('checked'),
-          flag_ = true;
-        if (curName === allName) {
-          $this.find('input[type="checkbox"][name="' + targetName + '"]').prop('checked', flag)
-        } else {
-          $this.find('input[type="checkbox"][name="' + targetName + '"]').each(function (i, dom) {
-            !$(dom).prop('checked') && (flag_ = false);
-          });
-          $this.find('input[type="checkbox"][name="' + allName + '"]').prop('checked', flag_);
-
+    $(document).on('click.allSelect', '[data-toggle="allSelect"]', function (ev) {
+        var $this = $(this),
+            clickTarget = $(ev.target);
+        if (clickTarget[0].tagName.toLowerCase() === 'input' && clickTarget.attr('type') === 'checkbox') {
+            var $thisDate = $this.data(),
+                num = -1,
+                tag = -1,
+                allName = $thisDate.allName.split(','),
+                targetName = $thisDate.targetName.split(','),
+                curName = clickTarget.attr('name');
+            if(curName){
+                var flag = clickTarget.prop('checked'),
+                    flag_ = true;
+                num = targetName.indexOf(curName);
+                if(num === -1){
+                    num = allName.indexOf(curName);
+                    if(num !== -1){
+                        tag = 1;
+                    }
+                }else{
+                    tag = 0;
+                }
+                if(num !== -1 && tag !== -1){
+                    switch(tag){
+                        case 0:
+                            $this.find('input[type="checkbox"][name="' + targetName[num] + '"]').each(function (i, dom) {
+                                !$(dom).prop('checked') && (flag_ = false);
+                            });
+                            $this.find('input[type="checkbox"][name="' + allName[num] + '"]').prop('checked', flag_);
+                            break;
+                        case 1:
+                            $this.find('input[type="checkbox"][name="' + targetName[num] + '"]').prop('checked', flag);
+                            break;
+                    }
+                }
+            }
         }
-      }
-    }
-  });
+    });
 })(jQuery);
 
 /**
